235a55acd5471cdadc7138e302629f42277c5eb5
fredvs <fiens@hotmail.com>
2021-03-16 18:16:10 +0100

    Some modifs in demo.

--- src/alsa_mixer_test.pas
+++ src/alsa_mixer_test.pas
@@ -30,22 +30,30 @@ begin
   writeln('Original Volume left = ' + IntToStr(volleft) + '/100');
   writeln('Original Volume right = ' + IntToStr(volright) + '/100');
   writeln();
+  
+  sleep(300);
 
-  ALSAmixerSetVolume(0, 75);
+  ALSAmixerSetVolume(0, 25);
   ALSAmixerSetVolume(1, 25);
 
   writeln('New Volume left = ' + IntToStr(ALSAmixerGetVolume(0)) + '/100');
   writeln('New Volume right = ' + IntToStr(ALSAmixerGetVolume(1)) + '/100');
+  
+  sleep(1500);
 
   writeln();
 
   ALSAmixerSetVolume(0, volleft);
   ALSAmixerSetVolume(1, volright);
-
-  writeln('Back Volume left = ' + IntToStr(ALSAmixerGetVolume(0)) + '/100');
-  writeln('Back Volume right = ' + IntToStr(ALSAmixerGetVolume(1)) + '/100');
+  
+ 
+  writeln('Back to original Volume left = ' + IntToStr(ALSAmixerGetVolume(0)) + '/100');
+  writeln('Back to original Volume right = ' + IntToStr(ALSAmixerGetVolume(1)) + '/100');
   //}
   writeln();
+  
+  sleep(300);
+  
   writeln('Change volume from mixer-system to fire the callback...');  // not yet working
   writeln();
   writeln('Press a key to quit.');
